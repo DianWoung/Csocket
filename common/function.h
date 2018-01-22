025395265aa286ff33370828e32d04945f08aa8a
Dian Wang <dian126@foxmail.com>
2018-01-22 08:58:10 +0000

    chapter10 multiprocess server

--- common/function.h
+++ common/function.h
@@ -4,3 +4,13 @@ void error_handling(char *message)
     fputc('\n', stderr);
     exit(1);
 }
+
+void read_childproc(int sig)
+{
+    int status;
+    pid_t id = waitpid(-1, &status, WNOHANG);
+    if (WIFEXITED(status)){
+        printf("Remove proc id: %d \n", id);
+        printf("Child send: %d \n", WEXITSTATUS(status));
+    }
+}

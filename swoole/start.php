55ab25510f0a7d31431480c6268fce8423046673
Dian Wang <dian126@foxmail.com>
2018-02-06 02:11:06 +0000

    backup the files

--- swoole/start.php
+++ swoole/start.php
@@ -1,28 +1,18 @@
 <?php
-//php在线直播示例代码
-//使用PHPCLI模式运行
-//命令：php start.php
-
-//设置路径
-define('_ROOT_', dirname(__FILE__));
-require_once _ROOT_.'/function.php';
-//监听地址和端口
 $server = new swoole_websocket_server("0.0.0.0", 88);
-//服务端接收连接事件
+
 $server->on('open', function (swoole_websocket_server $server, $request) {
-    if(!file_exists(_ROOT_.'/client/'.$request->fd.'.client')){
-        @file_put_contents(_ROOT_.'/client/'.$request->fd.'.client',$request->fd);
-    }
+    echo "server: handshake success with fd{$request->fd}\n";
 });
-//服务端接收信息事件
+
 $server->on('message', function (swoole_websocket_server $server, $frame) {
-    foreach(notice(_ROOT_.'/client/') as $v){
-            $server->push($v,$frame->data);
-    }
+	foreach($server->connection_list() as $fd) {
+		$server->push($fd, $frame->data,true);
+	}
 });
-//服务端接收关闭事件
+
 $server->on('close', function ($ser, $fd) {
-    @unlink(_ROOT_.'/client/'.$fd.'.client');
+    echo "client {$fd} closed\n";
 });
-//服务开启
+
 $server->start();
---
+++ workspace/struct/helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define R 10
+int main(int argc, char *argv[])
+{
+	int a = R;
+    printf("Hello world a = %d\n", a);
+    return 0;
+}
